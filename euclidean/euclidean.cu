a380236c8aa6c73f5659b7907887f0499f9e284a
poonkuzhali <poonkuzhali1996@gmail.com>
2024-12-01 17:09:00 -0500

    Added serial implementation

--- euclidean/euclidean.cu
+++ euclidean/euclidean.cu
@@ -4,44 +4,54 @@
 #include<cuda_runtime.h>
 #include<math.h>
 
-void InitArray(int *array, int length) {
-    for(int i=0; i<length; i++) {
-        array[i] = rand() % 100;
+void InitArray(float *array, int length) {
+    for(int i = 0; i < length; i++) {
+        array[i] = static_cast<float>(rand() % 100);
     }
 }
 
-__global__ void computeSquares(const int *a, const int *b, int *sq_diff, int length) {
+double serialEuclidean(const float *a, const float *b, int length) {
+    double total = 0.0f;
+
+    for(int i=0; i<length; i++) {
+        double diff = static_cast<double>(a[i]) - static_cast<double>(b[i]);
+        total += diff * diff;
+    }
+
+    return static_cast<double>(sqrt(total));
+}
+
+__global__ void computeSquares(const float *a, const float *b, float *sq_diff, int length) {
     //global id = block id in grid * total threads in a block + thread id within a block
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (idx < length) {
-        int diff = a[idx] - b[idx];
+        float diff = a[idx] - b[idx];
         sq_diff[idx] = diff * diff;
     }
 }
 
-__global__ void reduce(int *input, int *output, int length) {
-    extern __shared__ int shared_mem[];
+__global__ void reduce(float *input, float *output, int length) {
+    extern __shared__ float shared_mem[];
     int thread_id = threadIdx.x;
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if(idx < length) {
-        shared_mem[thread_id] = input[idx]; 
+    if (idx < length) {
+        shared_mem[thread_id] = input[idx];
     } else {
-        shared_mem[thread_id] = 0;
+        shared_mem[thread_id] = 0.0f;
     }
     __syncthreads();
 
-    for(int x=blockDim.x/2; x > 0; x /=2) {
-        if(thread_id < x) {
+    for (int x = blockDim.x / 2; x > 0; x /= 2) {
+        if (thread_id < x) {
             shared_mem[thread_id] += shared_mem[thread_id + x];
         }
-
         __syncthreads();
     }
 
     //final result for the block sum from shared_mem[0] copied to output
-    if(thread_id == 0) {
+    if (thread_id == 0) {
         output[blockIdx.x] = shared_mem[0];
     }
 }
@@ -56,16 +66,16 @@ int main(int argc, char **argv) {
     const int blocksPerGrid = (length + threadsPerBlock - 1)/threadsPerBlock;
 
     // host arrays
-    int *h_a = (int*)malloc(length * sizeof(int));
-    int *h_b = (int*)malloc(length * sizeof(int));
+    float *h_a = (float*)malloc(length * sizeof(float));
+    float *h_b = (float*)malloc(length * sizeof(float));
 
     // device arrays
-    int *d_a;
-    int *d_b;
-    int *d_result;
-    cudaMalloc((void**)&d_a, length * sizeof(int));
-    cudaMalloc((void**)&d_b, length * sizeof(int));
-    cudaMalloc((void**)&d_result, length * sizeof(int));
+    float *d_a;
+    float *d_b;
+    float *d_result;
+    cudaMalloc((void**)&d_a, length * sizeof(float));
+    cudaMalloc((void**)&d_b, length * sizeof(float));
+    cudaMalloc((void**)&d_result, length * sizeof(float));
 
     cudaEvent_t cuda_start;
     cudaEvent_t cuda_stop;
@@ -97,42 +107,49 @@ int main(int argc, char **argv) {
     printf("Prop 3: Total global memory: %zu\n", device_properties.totalGlobalMem);
     printf("Prop 4: Shared memory per block: %zu\n", device_properties.sharedMemPerBlock);
     
-    //Initialize host arrays
+    // Initialize host arrays
     InitArray(h_a, length);
     InitArray(h_b, length);
 
-    //Copy host input arrays to device
-    cudaMemcpy(d_a, h_a, length * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, h_b, length * sizeof(int), cudaMemcpyHostToDevice);
+    // Copy host input arrays to device
+    cudaMemcpy(d_a, h_a, length * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, length * sizeof(float), cudaMemcpyHostToDevice);
 
     //calling CUDA kernel
     cudaEventRecord(cuda_start);
     computeSquares<<<blocksPerGrid, threadsPerBlock>>>(d_a, d_b, d_result, length);
     cudaDeviceSynchronize();
 
-    //Parallel reduce
-    int *d_partial;
-    cudaMalloc((void**)&d_partial, blocksPerGrid * sizeof(int));
-    reduce<<<blocksPerGrid, threadsPerBlock, threadsPerBlock * sizeof(int)>>>(d_result, d_partial, length);
+    // Parallel reduce
+    float *d_partial;
+    cudaMalloc((void**)&d_partial, blocksPerGrid * sizeof(float));
+    reduce<<<blocksPerGrid, threadsPerBlock, threadsPerBlock * sizeof(float)>>>(d_result, d_partial, length);
     cudaDeviceSynchronize();
 
-    int *h_partial = (int*)malloc(blocksPerGrid * sizeof(int));
-    cudaMemcpy(h_partial, d_partial, blocksPerGrid * sizeof(int), cudaMemcpyDeviceToHost);
-    cudaEventRecord(cuda_stop);
+    float *h_partial = (float*)malloc(blocksPerGrid * sizeof(float));
+    cudaMemcpy(h_partial, d_partial, blocksPerGrid * sizeof(float), cudaMemcpyDeviceToHost);
 
+    cudaEventRecord(cuda_stop);
     cudaEventSynchronize(cuda_stop);
 
     float cuda_time;
     cudaEventElapsedTime(&cuda_time, cuda_start, cuda_stop);
     printf("CUDA Time: %.6f s\n", cuda_time/1000.0f);
 
-    int total = 0;
+    float total = 0.0f;
     for(int i=0; i<blocksPerGrid; i++) {
         total += h_partial[i];
     }
 
-    float euclidean_dist = sqrt(total);
-    printf("Euclidean distance: %.6f\n", euclidean_dist);
+    double euclidean_dist = sqrt(total);
+    printf("Euclidean distance - CUDA: %.6f\n", euclidean_dist);
+
+    //Serial Euclidean
+    clock_t start = clock();
+    float serial_euc = serialEuclidean(h_a, h_b, length);
+    clock_t end = clock();
+    printf("Serial Time: %.6f s\n", (double)(end - start) / CLOCKS_PER_SEC);
+    printf("Euclidean distance - Serial: %.6f\n", serial_euc);
 
     free(h_a);
     free(h_b);
